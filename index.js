db2c0652df4cc42ff1348ebedebe8ffa7d6f9e64
jaewoogwak <jaewoo010207@gmail.com>
2022-05-23 14:38:41 +0900

    innerHTML -> innerText

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const getCityWeatherData = async (cityName) => {
   const temp = document.getElementsByClassName("temp")[0];
   const weather = document.getElementsByClassName("weather")[0];
   const city = document.getElementsByClassName("city")[0];
-  city.innerHTML = "Loading...";
-  weather.innerHTML = "Loading...";
-  temp.innerHTML = "Loading...";
+  city.innerText = "Loading...";
+  weather.innerText = "Loading...";
+  temp.innerText = "Loading...";
   const data = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0be87df02cd14b1d08489cd0372b5b0e`
   );
-  city.innerHTML = `${cityName}`;
-  weather.innerHTML = `${data.data.weather[0].main}`;
-  temp.innerHTML = `${(data.data.main.feels_like - 273.15).toFixed(1)}도`;
+  city.innerText = `${cityName}`;
+  weather.innerText = `${data.data.weather[0].main}`;
+  temp.innerText = `${(data.data.main.feels_like - 273.15).toFixed(1)}도`;
 };
 
 const setCity = () => {
